7cfe4c50fac786154598be7b1295060b7a6d4df4
liora <lioravraham7@gmail.com>
2025-05-15 11:46:18 +0300

    commit 1.1

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,18 @@
 package pages;
 
-public class BasePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage {
+	protected WebDriver driver;
+	
+	public BasePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	protected void click(By locator) {
+		
+	}
+	
 	
 }
